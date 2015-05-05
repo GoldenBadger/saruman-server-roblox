df735d6b711b29a5387f6117bc2ebae1757d0490
Terry Bolt <bolt.terry@gmail.com>
2015-05-05 17:48:36 +0100

    Made change which should hopefully fix the zombie issue.

--- api/chess_engine_pool.py
+++ api/chess_engine_pool.py
@@ -70,6 +70,7 @@ class ChessEnginePool(object):
         while engine_cancel.value == 0:
             if engine_process.poll() != None:
                 # Then the engine process has died. Restart it.
+                del(engine_process)
                 engine_process = engine_process = Popen(
                     engine_filename, stdin=PIPE, stdout=PIPE,
                     universal_newlines=True, bufsize=1)
